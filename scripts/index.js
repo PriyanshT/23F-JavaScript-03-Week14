194b45816cc79e161c35b4d176b9322bab6aafea
Priyansh Thakar <priyanshthakar@gmail.com>
2023-12-04 20:26:49 -0500

    added td data

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,36 @@ function fetchDataFromJson() {
 function displayData(json) {
     console.log(json);
 
+    const movieData = json.Search;
     // looping through each object of the array
+    for (let i = 0; i < movieData.length; i++) {
+        console.log(movieData[i]);
+
+        // create tr and td elements for the body tag
+        // <tr>
+        //   <td>id</th>
+        //   <td>poster</td>
+        //   <td>name</td>
+        //   <td>year</td>
+        // </tr>
+        let tableRow = document.createElement("tr"); // <tr></tr>
+        let titleData = document.createElement("td"); // <td></td>
+        let yearData = document.createElement("td"); // <td></td>
+        let imdbIdData = document.createElement("td"); // <td></td>
+        let posterData = document.createElement("td"); // <td></td>
+
+        // add data in all the td tags
+        titleData.textContent = movieData[i].Title;
+        yearData.textContent = movieData[i].Year;
+        imdbIdData.textContent = movieData[i].imdbID;
+
+        // appending all the data to tr tags and adding tr to tbody tag
+        tableRow.appendChild(imdbIdData);
+        tableRow.appendChild(titleData);
+        tableRow.appendChild(yearData);
+
+        tableBody.appendChild(tableRow);
+    }
 }
 
 // add event listener on the submit button to call fetchDataFromJson fuction
